a2dd15c165e0fb95194830dc258bd457a750b7ae
agadrius <lessire.florent@gmail.com>
2017-04-28 12:50:43 +0100

    creation du footer

---
+++ include/footer.php
@@ -0,0 +1,24 @@
+<footer class="row" id="footer">
+	<div class="container">
+		<div class="col-md-3">
+			<h3>Coordonnées</h3>
+			<p>Light-code</br>
+			Place Paul Dubois 198</br>
+			<em>5550</em> <strong>sugny</strong></br>
+			+32(0)478/26.07.52</br>
+			tva </br></p>
+		</div>
+		<div class="col-md-offset-6 col-md-3">
+			<h3>Nous rejoindre</h3>
+			<div class= "row">
+				<div class="col-lg-12" id="logo_fb">
+					<a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a><!-- logo fb -->
+				</div>
+				<div class="col-lg-12" id="btn_fb">
+					<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fspeedmovetaxi%2F&width=64&layout=box_count&action=like&size=small&show_faces=true&share=true&height=65&appId" width="64" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</footer>
