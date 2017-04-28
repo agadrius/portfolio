ba93eeddefe376d44342f46cbe64dc9f885e71d3
agadrius <lessire.florent@gmail.com>
2017-04-28 12:46:10 +0100

    ajout du carrousel sans images

---
+++ include/slider.php
@@ -0,0 +1,15 @@
+<div class="row hidden-xs" id="carousel">
+	<div id="carousel" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+		<div class="item active"> <img alt="" src=""></div>
+		<div class="item"> <img alt="" src=""></div>
+		<div class="item"> <img alt="" src=""></div>
+	</div>
+		<a class="left carousel-control" href="#carousel" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		</a>
+		<a class="right carousel-control" href="#carousel" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+	</div>
+</div>
