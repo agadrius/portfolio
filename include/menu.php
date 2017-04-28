a1b376be49218348edafc78d5449922c9cb48021
agadrius <lessire.florent@gmail.com>
2017-04-28 12:48:59 +0100

    ajout du menu

---
+++ include/menu.php
@@ -0,0 +1,28 @@
+<div class="row" id="menu">
+	<nav class="navbar navbar-default">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		</div>
+		<div id="navbar" class="collapse navbar-collapse">
+		  <ul class="nav navbar-nav" >
+			<li><a href="">Accueil</a></li>
+			<li class="dropdown"> 
+				<a data-toggle="dropdown" href="#">Services<b class="caret"></b></a>
+				<ul class="dropdown-menu">
+				  <li><a href="">créartion de site web</a></li>
+				  <li><a href="">réparation pc</a></li>
+				  <li><a href="">montage pc</a></li>
+				</ul>
+			  </li>
+			<li><a href="">portfolio</a></li>
+			<li><a href="">Devis</a></li>
+			<li><a href="">Contact</a></li>
+		  </ul>
+		</div><!--/.nav-collapse -->
+	</nav>
+</div>
