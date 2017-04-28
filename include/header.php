6b986ab70e93cb0297dfbe4ea30cb8f011a9192a
agadrius <lessire.florent@gmail.com>
2017-04-28 13:15:16 +0100

    mise en forme div header titre+phrase tel

--- include/header.php
+++ include/header.php
@@ -1,5 +1,9 @@
 <header class="row">
-	<h1>LESSIRE Florent</h1>
-	<p>dévellopper full-stack, maintenance et montage pc</p>
-	<a href="">0478/26.07.52</a>
+	<div class="col-md-8">
+		<h1>LESSIRE Florent</h1>
+		<p>dévellopper full-stack, maintenance et montage pc</p>
+	</div>
+	<div class="col-md-4">
+		<a href="">0478/26.07.52</a>
+	</div>
 </header>
