aeb90a03ca405f60b2151644a89706ae70cf64d3
agadrius <lessire.florent@gmail.com>
2017-04-28 13:31:09 +0100

    lien tel ok

--- include/header.php
+++ include/header.php
@@ -4,6 +4,6 @@
 		<p>dévellopper full-stack, maintenance et montage pc</p>
 	</div>
 	<div class="col-md-4">
-		<a href="">0478/26.07.52</a>
+		<a href="tel:0478260752">0478/26.07.52</a>
 	</div>
 </header>
